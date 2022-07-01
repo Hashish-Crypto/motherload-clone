3d6e68b7a2b30106f53c3a1a40ade06599bae5f5
Acquati <leandroacquati@gmail.com>
2022-07-01 10:45:16 -0300

    add layers of ground itens

--- assets/Scripts/lib/generateGroundGrid.ts
+++ assets/Scripts/lib/generateGroundGrid.ts
@@ -38,40 +38,193 @@ const generateGroundGrid = (
         } else {
           loadGround(GroundItems.grass, groundGrid[y][x])
         }
-      } else {
-        const randomNumber = randomRangeInt(0, 40)
-        if (randomNumber === 0) {
+      } else if (y >= groundGridHeight - 11) {
+        const randomNumber = randomRangeInt(0, 50)
+        if (randomNumber < 5) {
           loadGround(GroundItems.ironium, groundGrid[y][x])
-        } else if (randomNumber === 1) {
+        } else if (randomNumber < 8) {
           loadGround(GroundItems.bronzium, groundGrid[y][x])
-        } else if (randomNumber === 2) {
+        } else if (randomNumber < 9) {
           loadGround(GroundItems.silverium, groundGrid[y][x])
-        } else if (randomNumber === 3) {
-          loadGround(GroundItems.goldium, groundGrid[y][x])
-        } else if (randomNumber === 4) {
-          loadGround(GroundItems.platinium, groundGrid[y][x])
-        } else if (randomNumber === 5) {
-          loadGround(GroundItems.einsteinium, groundGrid[y][x])
-        } else if (randomNumber === 6) {
-          loadGround(GroundItems.emerald, groundGrid[y][x])
-        } else if (randomNumber === 7) {
-          loadGround(GroundItems.ruby, groundGrid[y][x])
-        } else if (randomNumber === 8) {
-          loadGround(GroundItems.diamond, groundGrid[y][x])
-        } else if (randomNumber === 9) {
-          loadGround(GroundItems.amazonite, groundGrid[y][x])
-        } else if (randomNumber === 10) {
-          loadGround(GroundItems.dinosaurBones, groundGrid[y][x])
-        } else if (randomNumber === 11) {
-          loadGround(GroundItems.treasure, groundGrid[y][x])
-        } else if (randomNumber === 12) {
-          loadGround(GroundItems.martianSkeleton, groundGrid[y][x])
-        } else if (randomNumber === 13) {
-          loadGround(GroundItems.religiousArtifact, groundGrid[y][x])
-        } else if (randomNumber === 14) {
+        } else if (randomNumber < 13) {
           loadGround(GroundItems.rock, groundGrid[y][x])
-        } else if (randomNumber === 15) {
+        } else if (randomNumber < 22) {
+          groundGrid[y][x].active = false
+          groundGrid[y][x].node.destroy()
+        } else {
+          loadGround(GroundItems.ground, groundGrid[y][x])
+        }
+      } else if (y >= groundGridHeight - 21) {
+        const randomNumber = randomRangeInt(0, 50)
+        if (randomNumber < 5) {
+          loadGround(GroundItems.bronzium, groundGrid[y][x])
+        } else if (randomNumber < 8) {
+          loadGround(GroundItems.silverium, groundGrid[y][x])
+        } else if (randomNumber < 9) {
+          loadGround(GroundItems.goldium, groundGrid[y][x])
+        } else if (randomNumber < 14) {
+          loadGround(GroundItems.rock, groundGrid[y][x])
+        } else if (randomNumber < 22) {
+          groundGrid[y][x].active = false
+          groundGrid[y][x].node.destroy()
+        } else if (randomNumber < 23) {
+          loadGround(GroundItems.dinosaurBones, groundGrid[y][x])
+        } else {
+          loadGround(GroundItems.ground, groundGrid[y][x])
+        }
+      } else if (y >= groundGridHeight - 31) {
+        const randomNumber = randomRangeInt(0, 50)
+        if (randomNumber < 5) {
+          loadGround(GroundItems.silverium, groundGrid[y][x])
+        } else if (randomNumber < 8) {
+          loadGround(GroundItems.goldium, groundGrid[y][x])
+        } else if (randomNumber < 9) {
+          loadGround(GroundItems.platinium, groundGrid[y][x])
+        } else if (randomNumber < 15) {
+          loadGround(GroundItems.rock, groundGrid[y][x])
+        } else if (randomNumber < 22) {
+          groundGrid[y][x].active = false
+          groundGrid[y][x].node.destroy()
+        } else if (randomNumber < 23) {
+          loadGround(GroundItems.dinosaurBones, groundGrid[y][x])
+        } else {
+          loadGround(GroundItems.ground, groundGrid[y][x])
+        }
+      } else if (y >= groundGridHeight - 41) {
+        const randomNumber = randomRangeInt(0, 50)
+        if (randomNumber < 5) {
+          loadGround(GroundItems.goldium, groundGrid[y][x])
+        } else if (randomNumber < 8) {
+          loadGround(GroundItems.platinium, groundGrid[y][x])
+        } else if (randomNumber < 9) {
+          loadGround(GroundItems.einsteinium, groundGrid[y][x])
+        } else if (randomNumber < 15) {
+          loadGround(GroundItems.rock, groundGrid[y][x])
+        } else if (randomNumber < 22) {
+          groundGrid[y][x].active = false
+          groundGrid[y][x].node.destroy()
+        } else if (randomNumber < 23) {
           loadGround(GroundItems.lava, groundGrid[y][x])
+        } else if (randomNumber < 24) {
+          loadGround(GroundItems.treasure, groundGrid[y][x])
+        } else {
+          loadGround(GroundItems.ground, groundGrid[y][x])
+        }
+      } else if (y >= groundGridHeight - 51) {
+        const randomNumber = randomRangeInt(0, 50)
+        if (randomNumber < 5) {
+          loadGround(GroundItems.platinium, groundGrid[y][x])
+        } else if (randomNumber < 8) {
+          loadGround(GroundItems.einsteinium, groundGrid[y][x])
+        } else if (randomNumber < 9) {
+          loadGround(GroundItems.emerald, groundGrid[y][x])
+        } else if (randomNumber < 15) {
+          loadGround(GroundItems.rock, groundGrid[y][x])
+        } else if (randomNumber < 22) {
+          groundGrid[y][x].active = false
+          groundGrid[y][x].node.destroy()
+        } else if (randomNumber < 24) {
+          loadGround(GroundItems.lava, groundGrid[y][x])
+        } else if (randomNumber < 25) {
+          loadGround(GroundItems.treasure, groundGrid[y][x])
+        } else {
+          loadGround(GroundItems.ground, groundGrid[y][x])
+        }
+      } else if (y >= groundGridHeight - 61) {
+        const randomNumber = randomRangeInt(0, 50)
+        if (randomNumber < 5) {
+          loadGround(GroundItems.einsteinium, groundGrid[y][x])
+        } else if (randomNumber < 7) {
+          loadGround(GroundItems.emerald, groundGrid[y][x])
+        } else if (randomNumber < 8) {
+          loadGround(GroundItems.ruby, groundGrid[y][x])
+        } else if (randomNumber < 15) {
+          loadGround(GroundItems.rock, groundGrid[y][x])
+        } else if (randomNumber < 22) {
+          groundGrid[y][x].active = false
+          groundGrid[y][x].node.destroy()
+        } else if (randomNumber < 25) {
+          loadGround(GroundItems.lava, groundGrid[y][x])
+        } else if (randomNumber < 26) {
+          loadGround(GroundItems.martianSkeleton, groundGrid[y][x])
+        } else {
+          loadGround(GroundItems.ground, groundGrid[y][x])
+        }
+      } else if (y >= groundGridHeight - 71) {
+        const randomNumber = randomRangeInt(0, 50)
+        if (randomNumber < 3) {
+          loadGround(GroundItems.emerald, groundGrid[y][x])
+        } else if (randomNumber < 4) {
+          loadGround(GroundItems.ruby, groundGrid[y][x])
+        } else if (randomNumber < 5) {
+          loadGround(GroundItems.diamond, groundGrid[y][x])
+        } else if (randomNumber < 15) {
+          loadGround(GroundItems.rock, groundGrid[y][x])
+        } else if (randomNumber < 22) {
+          groundGrid[y][x].active = false
+          groundGrid[y][x].node.destroy()
+        } else if (randomNumber < 26) {
+          loadGround(GroundItems.lava, groundGrid[y][x])
+        } else if (randomNumber < 27) {
+          loadGround(GroundItems.martianSkeleton, groundGrid[y][x])
+        } else {
+          loadGround(GroundItems.ground, groundGrid[y][x])
+        }
+      } else if (y >= groundGridHeight - 81) {
+        const randomNumber = randomRangeInt(0, 50)
+        if (randomNumber < 2) {
+          loadGround(GroundItems.ruby, groundGrid[y][x])
+        } else if (randomNumber < 3) {
+          loadGround(GroundItems.diamond, groundGrid[y][x])
+        } else if (randomNumber < 4) {
+          loadGround(GroundItems.amazonite, groundGrid[y][x])
+        } else if (randomNumber < 15) {
+          loadGround(GroundItems.rock, groundGrid[y][x])
+        } else if (randomNumber < 22) {
+          groundGrid[y][x].active = false
+          groundGrid[y][x].node.destroy()
+        } else if (randomNumber < 27) {
+          loadGround(GroundItems.lava, groundGrid[y][x])
+        } else if (randomNumber < 28) {
+          loadGround(GroundItems.religiousArtifact, groundGrid[y][x])
+        } else {
+          loadGround(GroundItems.ground, groundGrid[y][x])
+        }
+      } else if (y >= groundGridHeight - 91) {
+        const randomNumber = randomRangeInt(0, 50)
+        if (randomNumber < 1) {
+          loadGround(GroundItems.diamond, groundGrid[y][x])
+        } else if (randomNumber < 2) {
+          loadGround(GroundItems.amazonite, groundGrid[y][x])
+        } else if (randomNumber < 3) {
+          loadGround(GroundItems.lava, groundGrid[y][x])
+        } else if (randomNumber < 15) {
+          loadGround(GroundItems.rock, groundGrid[y][x])
+        } else if (randomNumber < 22) {
+          groundGrid[y][x].active = false
+          groundGrid[y][x].node.destroy()
+        } else if (randomNumber < 29) {
+          loadGround(GroundItems.lava, groundGrid[y][x])
+        } else if (randomNumber < 30) {
+          loadGround(GroundItems.religiousArtifact, groundGrid[y][x])
+        } else {
+          loadGround(GroundItems.ground, groundGrid[y][x])
+        }
+      } else {
+        const randomNumber = randomRangeInt(0, 50)
+        if (randomNumber < 3) {
+          loadGround(GroundItems.amazonite, groundGrid[y][x])
+        } else if (randomNumber < 8) {
+          loadGround(GroundItems.lava, groundGrid[y][x])
+        } else if (randomNumber < 15) {
+          loadGround(GroundItems.rock, groundGrid[y][x])
+        } else if (randomNumber < 22) {
+          groundGrid[y][x].active = false
+          groundGrid[y][x].node.destroy()
+        } else if (randomNumber < 30) {
+          loadGround(GroundItems.lava, groundGrid[y][x])
+        } else if (randomNumber < 31) {
+          loadGround(GroundItems.religiousArtifact, groundGrid[y][x])
         } else {
           loadGround(GroundItems.ground, groundGrid[y][x])
         }
